674bce75ad11482f5355931f041b15ada5148d0c
irum-shah78 <irams4812@gmail.com>
2024-05-01 18:34:20 +0500

    header section

--- script.js
+++ script.js
@@ -11,5 +11,31 @@ menuBtn.addEventListener("click", (e) =>{
 
 navLinks.addEventListener("click", (e) =>{
    navLinks.classList.remove("open");
-   menuIcon.setAttribute("class", "fa-solid fa-bars")
-})
+   menuIcon.setAttribute("class", "fa-solid fa-bars");
+});
+
+const scrollRevealOption = {
+  distance: "50px",
+  duration: "bottom",
+  duration: 1000
+};
+
+ScrollReveal().reveal(".header_image img", {
+  ...scrollRevealOption,
+  origin: "right",
+});
+
+ScrollReveal().reveal(".header_content h1", {
+  ...scrollRevealOption,
+  origin: "right",
+});
+
+ScrollReveal().reveal(".header_content .section_description", {
+  ...scrollRevealOption,
+  delay: 1000,
+});
+
+ScrollReveal().reveal(".header_content .header_btn", {
+  ...scrollRevealOption,
+  delay: 1500,
+});
