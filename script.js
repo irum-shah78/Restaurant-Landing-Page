d13e2ce3667a14282acf187792a526e936a81fdd
irum-shah78 <irams4812@gmail.com>
2024-05-03 08:54:54 +0500

    Chef section

--- script.js
+++ script.js
@@ -64,3 +64,24 @@ ScrollReveal().reveal(".banner_card", {
   ...scrollRevealOption,
   interval: 500,
 });
+
+ScrollReveal().reveal(".chef_img img", {
+  ...scrollRevealOption,
+  origin: "right",
+});
+
+ScrollReveal().reveal(".chef_content .section_header", {
+  ...scrollRevealOption,
+  delay: 500,
+});
+
+ScrollReveal().reveal(".chef_content .section_description", {
+  ...scrollRevealOption,
+  delay: 1000,
+});
+
+ScrollReveal().reveal(".chef_list li", {
+  ...scrollRevealOption,
+  delay: 1500,
+  interval: 500,
+});
